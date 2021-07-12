6f57a029c355f511b3604fde259ff9e825e90304
SaiArun-ai <72151107+SaiArun-ai@users.noreply.github.com>
2021-07-12 20:26:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,9 +2,25 @@ function preset(){
 
 }
 function setup(){
-    Canvas = createCanvas(300,300);
-    Canvas.center();
+    canvas = createCanvas(300,300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/LRPJPbhq7/model.json",ML);
+}
+function ML(){
+    console.log("Model Loaded");
 }
 function draw(){
-
+    image(video,0,0,300,300);
+    classifier.classify(video,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }else{
+        console.log(results[0].label);
+        document.getElementById("UON").innerHTML = results[0].label;
+        document.getElementById("UOA").innerHTML = results[0].confidence.toFixed(5);
+    }
 }
